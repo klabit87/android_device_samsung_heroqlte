c919bb8713a5c59d99e4118dca3c31033c746ea0
klabit87 <keith.labit@gmail.com>
2017-09-28 21:21:15 -0500

    Adapt for lineage

---
+++ lineage.mk
@@ -0,0 +1,6 @@
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+
+$(call inherit-product, device/samsung/heroqlte/full_heroqlte.mk)
+
+PRODUCT_DEVICE := heroqlte
+PRODUCT_NAME := lineage_heroqlte
